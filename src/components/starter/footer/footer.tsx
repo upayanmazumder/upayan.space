f4682d9f4cfc8346fe052652a63545881513321c
g9Aerospace <g9militants@gmail.com>
2024-05-11 20:37:17 +0530

    Fixed href of footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable qwik/jsx-a */
 import { component$ } from "@builder.io/qwik";
 import styles from "./footer.module.css";
 
 export default component$(() => {
   return (
     <footer class={styles.footer}>
-      <a href="#" class={styles.anchor}>
+      <a class={styles.anchor}>
         <span>©️ upayan.space</span>
       </a>
     </footer>
