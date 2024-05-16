abfdbd87df2c8cd6ce128781dc2a8696520c861a
upayanmazumder <upayanm3@gmail.com>
2024-05-16 23:56:20 +0530

    Improvements

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,10 +1,25 @@
 import { component$ } from '@builder.io/qwik';
-import blog from "./blog.module.css"
+import type { DocumentHead } from "@builder.io/qwik-city";
+import blog from "./blog.module.css";
+
 export default component$(() => {
   return (
+    <>
     <div class={blog.introduction}>
         <h1>Welcome to my Blog</h1>
-        <p>Discover insightful articles, tutorials, and more!</p>
+        <p>Find out what I am up to!</p>
     </div>
+    <div role="presentation" class="ellipsis"></div>
+    </>
   );
 });
+
+export const head: DocumentHead = {
+  title: "Upayan | Blog",
+  meta: [
+    {
+      name: "description",
+      content: "Visit my blog!",
+    },
+  ],
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-
 import Hero from "../components/hero/hero";
 
 export default component$(() => {
   return (
+    <>
+    <div role="presentation" class="ellipsis"></div>
     <div class="override-background">
       <Hero />
     </div>
+    </>
   );
 });
 
