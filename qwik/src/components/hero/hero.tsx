aed2e9918b90639758ec7aa5926a394243a8d97a
Upayan Mazumder <upayanm3@gmail.com>
2024-08-05 19:29:42 +0530

    feat: Add social media links to hero component
    - Created heroLinks.json file to store social media links.
    - Added buttons for IIT-Madras, LinkedIn, Github, Discord, Gravatar, Instagram, Facebook, and Pinterest.
    - Each button has a corresponding class, name, and link.

--- qwik/src/components/hero/hero.tsx
+++ qwik/src/components/hero/hero.tsx
@@ -1,519 +1,84 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $ } from "@builder.io/qwik";
 import styles from "./hero.module.css";
+import buttonDetails from "./heroLinks.json";
 
 export default component$(() => {
+  const shootConfetti = $(
+    async (link: string) => {
+      const defaults = {
+        spread: 360,
+        ticks: 70,
+        gravity: 0,
+        decay: 0.95,
+        startVelocity: 30,
+        colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
+        origin: {
+          x: 0.5,
+          y: 0.35,
+        },
+      };
+
+      function loadConfetti() {
+        return new Promise<(opts: any) => void>((resolve, reject) => {
+          if ((globalThis as any).confetti) {
+            return resolve((globalThis as any).confetti as any);
+          }
+          const script = document.createElement("script");
+          script.src =
+            "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
+          script.onload = () => resolve((globalThis as any).confetti as any);
+          script.onerror = reject;
+          document.head.appendChild(script);
+          script.remove();
+        });
+      }
+
+      const confetti = await loadConfetti();
+
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 80,
+          scalar: 1.2,
+        });
+
+        confetti({
+          ...defaults,
+          particleCount: 60,
+          scalar: 0.75,
+        });
+      }
+
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+      setTimeout(shoot, 300);
+      setTimeout(shoot, 400);
+
+      // Open website in new tab after animation
+      setTimeout(() => {
+        window.open(link, "_blank");
+      }, 990); // Adjust the delay time as needed
+    }
+  );
+
   return (
     <div class={[styles.hero]}>
-    <h1>
-      Hello! I'm
-      <br />
-      <span class="highlight">Upayan</span>
-    </h1>
+      <h1>
+        Hello! I'm
+        <br />
+        <span class="highlight">Upayan</span>
+      </h1>
       <div class={styles["button-group"]}>
-          {/*IIT-M Button*/}
+        {buttonDetails.map((button, index) => (
           <button
-            class={styles["IIT-M-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://ds.study.iitm.ac.in/student/24F2002974", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
+            key={index}
+            class={styles[button.class]}
+            onClick$={() => shootConfetti(button.link)}
           >
-            IIT-Madras
+            {button.name}
           </button>
-        {/*LinkedIn Button*/}
-          <button
-            class={styles["linkedin-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://www.linkedin.com/in/upayanmazumder/", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            LinkedIn
-          </button>
-        {/*Github Button*/}
-          <button
-            class={styles["github-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://github.com/upayanmazumder", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            Github
-          </button>
-        {/*Discord Button*/}
-          <button
-            class={styles["discord-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://discord.com/users/1240025366853193758", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            Discord
-          </button>
-        {/*Gravatar Button*/}
-          <button
-            class={styles["gravatar-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://gravatar.com/upayanmazumder", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            Gravatar
-          </button>
-        {/*Instagram Button*/}
-          <button
-            class={styles["instagram-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://www.instagram.com/_._upayan_._/", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            Instagram
-          </button>
-        {/*Facebook Button*/}
-          <button
-            class={styles["facebook-button"]}
-            onClick$={async () => {
-              const defaults = {
-                spread: 360,
-                ticks: 70,
-                gravity: 0,
-                decay: 0.95,
-                startVelocity: 30,
-                colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-                origin: {
-                  x: 0.5,
-                  y: 0.35,
-                },
-              };
-
-              function loadConfetti() {
-                return new Promise<(opts: any) => void>((resolve, reject) => {
-                  if ((globalThis as any).confetti) {
-                    return resolve((globalThis as any).confetti as any);
-                  }
-                  const script = document.createElement("script");
-                  script.src =
-                    "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                  script.onload = () =>
-                    resolve((globalThis as any).confetti as any);
-                  script.onerror = reject;
-                  document.head.appendChild(script);
-                  script.remove();
-                });
-              }
-
-              const confetti = await loadConfetti();
-
-              function shoot() {
-                confetti({
-                  ...defaults,
-                  particleCount: 80,
-                  scalar: 1.2,
-                });
-
-                confetti({
-                  ...defaults,
-                  particleCount: 60,
-                  scalar: 0.75,
-                });
-              }
-
-              setTimeout(shoot, 0);
-              setTimeout(shoot, 100);
-              setTimeout(shoot, 200);
-              setTimeout(shoot, 300);
-              setTimeout(shoot, 400);
-
-              // Open website in new tab after animation
-              setTimeout(() => {
-                window.open("https://www.facebook.com/upayan.mazumder", "_blank");
-              }, 990); // Adjust the delay time as needed
-            }}
-          >
-            Facebook
-          </button>
-        {/*Pinterest Button*/}
-        <button
-          class={styles["pinterest-button"]}
-          onClick$={async () => {
-            const defaults = {
-              spread: 360,
-              ticks: 70,
-              gravity: 0,
-              decay: 0.95,
-              startVelocity: 30,
-              colors: ["006ce9", "ac7ff4", "18b6f6", "713fc2", "ffffff"],
-              origin: {
-                x: 0.5,
-                y: 0.35,
-              },
-            };
-
-            function loadConfetti() {
-              return new Promise<(opts: any) => void>((resolve, reject) => {
-                if ((globalThis as any).confetti) {
-                  return resolve((globalThis as any).confetti as any);
-                }
-                const script = document.createElement("script");
-                script.src =
-                  "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
-                script.onload = () =>
-                  resolve((globalThis as any).confetti as any);
-                script.onerror = reject;
-                document.head.appendChild(script);
-                script.remove();
-              });
-            }
-
-            const confetti = await loadConfetti();
-
-            function shoot() {
-              confetti({
-                ...defaults,
-                particleCount: 80,
-                scalar: 1.2,
-              });
-
-              confetti({
-                ...defaults,
-                particleCount: 60,
-                scalar: 0.75,
-              });
-            }
-
-            setTimeout(shoot, 0);
-            setTimeout(shoot, 100);
-            setTimeout(shoot, 200);
-            setTimeout(shoot, 300);
-            setTimeout(shoot, 400);
-
-            // Open website in new tab after animation
-            setTimeout(() => {
-              window.open("https://in.pinterest.com/upayanmazumder/", "_blank");
-            }, 990); // Adjust the delay time as needed
-          }}
-        >
-          Pinterest
-        </button>
+        ))}
       </div>
     </div>
   );
