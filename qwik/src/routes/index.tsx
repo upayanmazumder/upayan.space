bc2a98759d4697bf3d9640d1cf9a89cbf178c25a
upayanmazumder <upayanm3@gmail.com>
2024-09-12 23:43:18 +0530

    update meta info

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -18,13 +18,34 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Upayan",
+  title: "Upayan - Portfolio",
   meta: [
     {
       name: "description",
-      content: "Portfolio Site",
+      content: "Upayan's Portfolio - Explore the work and skills of Upayan, aspiring data scientist and developer.",
+    },
+    {
+      name: "author",
+      content: "Upayan Mazumder",
+    },
+    {
+      name: "theme-color",
+      content: "#3877b4",
+    },
+    {
+      property: "og:title",
+      content: "Upayan - Portfolio",
+    },
+    {
+      property: "og:description",
+      content: "Explore the work and skills of Upayan Mazumder, aspiring data scientist and developer.",
+    },
+    {
+      name: "twitter:image",
+      content: "https://i.imgur.com/sBVSdUh.jpeg",
     },
   ],
 };
+
 
 export {useGuildStatistics};
