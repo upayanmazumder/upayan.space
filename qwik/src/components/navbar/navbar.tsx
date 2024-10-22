29b80f6b127f41489a3122a56076a34f9892eae1
upayanmazumder <upayanm3@gmail.com>
2024-10-23 00:10:37 +0530

    Refactor navbar component to use icons for navigation items

--- qwik/src/components/navbar/navbar.tsx
+++ qwik/src/components/navbar/navbar.tsx
@@ -1,21 +1,25 @@
 import { component$ } from '@builder.io/qwik';
 import styles from './navbar.module.css';
+import { BsHouse, BsPersonBadge, BsPatchCheck, BsBasket, BsPersonRolodex } from '@qwikest/icons/bootstrap';
 
 export default component$(() => {
-    const navItems = {
-        Home: '/',
-        About: '/#about',
-        Certificates: '/certificates',
-        Services: '/#services',
-        Contact: '/#contact'
-    };
+    const navItems = [
+        { name: 'Home', href: '/', icon: BsHouse },
+        { name: 'About', href: '/#about', icon: BsPersonBadge },
+        { name: 'Certificates', href: '/certificates', icon: BsPatchCheck },
+        { name: 'Services', href: '/#services', icon: BsBasket },
+        { name: 'Contact', href: '/#contact', icon: BsPersonRolodex }
+    ];
 
     return (
         <nav class={styles.nav}>
             <ul class={styles.navList}>
-                {Object.entries(navItems).map(([name, href]) => (
+                {navItems.map(({ name, href, icon: Icon }) => (
                     <li key={name} class={styles.navItem}>
-                        <a href={href} class={styles.navLink}>{name}</a>
+                        <a href={href} class={styles.navLink}>
+                            <Icon class={styles.navIcon} />{' '}
+                            {name}
+                        </a>
                     </li>
                 ))}
             </ul>
