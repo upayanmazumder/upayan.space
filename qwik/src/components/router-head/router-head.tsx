085160daea2403a354bb0b68c855f1019c7783a8
upayanmazumder <upayanm3@gmail.com>
2024-10-23 20:09:59 +0530

    Refactor RouterHead component to update meta tags for consistent design and improve SEO

--- qwik/src/components/router-head/router-head.tsx
+++ qwik/src/components/router-head/router-head.tsx
@@ -1,10 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
-
+import UpayanImg from "../../media/upayan.svg";
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
-
 export const RouterHead = component$(() => {
   const head = useDocumentHead();
   const loc = useLocation();
@@ -17,13 +16,12 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <meta name="theme-color" content="#3046399" />
-
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:url" content={loc.url.href} />
-      <meta name="twitter:image" content="https://i.imgur.com/sBVSdUh.jpeg" />
-      <meta name="twitter:description" content={head.meta.find(m => m.name === 'description')?.content || 'Welcome to Upayan\'s website'} />
+      <meta name="theme-color" content="#304639" />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={loc.url.href} />
+      <meta property="og:image" content={UpayanImg} />
+      <meta property="og:description" content={head.meta.find(m => m.name === 'description')?.content || 'Welcome to Upayan\'s website'} />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
