de79df22d9239862759ad3385285baec8748f448
upayanmazumder <upayanm3@gmail.com>
2024-11-06 14:01:10 +0530

    Refactor session component to implement loading state with useStore and useVisibleTask

--- qwik/src/components/auth/session/session.tsx
+++ qwik/src/components/auth/session/session.tsx
@@ -1,11 +1,30 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import { useSession } from '../../../routes/plugin@auth';
 import sessionStyles from "./session.module.css";
 import unknownPerson from "../../../media/authentication/unknown-person.png";
 
 export default component$(() => {
   const session = useSession();
+  const state = useStore({ isLoading: true });
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(() => {
+    if (session.value) {
+      state.isLoading = false;
+    } else {
+      const timer = setTimeout(() => {
+        state.isLoading = false;
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  });
+
   const isSignedIn = session.value?.user;
+
+  if (state.isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
