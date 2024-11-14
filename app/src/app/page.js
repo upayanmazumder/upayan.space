32bd34d5f40787831c3b1ba191ccfbf286fb6f8d
upayanmazumder <upayanm3@gmail.com>
2024-11-14 11:46:57 +0530

    Add Carousel component to showcase services; update Home page and footer with version info

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import SocialMediaWork from "../components/social-media/work/work";
 import Activity from "../components/activity/activity";
+import ServicesCarousel from "../components/carousel/carousel";
+import Carousel from "../components/carousel/carousel";
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <SocialMediaWork />
         <Activity />
+        <Carousel />
       </main>
     </div>
   );
--- app/src/components/footer/footer.js
+++ app/src/components/footer/footer.js
@@ -1,16 +1,17 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SocialMediaPersonal from "../social-media/personal/personal";
 import styles from './footer.module.css';
+import packageJson from '../../../../package.json';
 
 const Footer = () => {
-
   return (
     <div className={styles.footer}>
       <SocialMediaPersonal />
-      <a href="/p/terms-of-service" className={styles.link}>Terms of Service</a>
+      <a href="/p/terms-of-service" className={styles.link}>Terms of Service</a> <span>  |  </span>
       <a href="/p/privacy-policy" className={styles.link}>Privacy Policy</a>
+      <div className={styles.version}>Version: {packageJson.version}</div>
     </div>
   );
 };
