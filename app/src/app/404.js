60dcfc786638b18b9bbe70a8cb3524e07584c19f
upayanmazumder <upayanm3@gmail.com>
2024-11-14 13:45:21 +0530

    Added a 404 page (doesnt work)

---
+++ app/src/app/404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NotFoundPage from "../components/404/404"; // Import the 404 page
+
+const App = () => {
+  return (
+    <NotFoundPage />
+  );
+};
+
+export default App;
