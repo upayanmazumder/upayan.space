57ea4e3b6197c4384606c206f624853ea553fd31
upayanmazumder <upayanm3@gmail.com>
2024-11-14 12:47:11 +0530

    Add Certificates component and page to display user certificates; include styling and data management

---
+++ app/src/app/certificates/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Certificates from '../../components/certificates/certificates';
+
+const CertificatesPage = () => {
+  return (
+    <>
+        <Certificates />
+    </>
+  );
+};
+
+export default CertificatesPage;
---
+++ app/src/shared/certificatesData.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+// Function to create slugs from titles
+export const createSlug = (title) => title.toLowerCase().replace(/\s+/g, '-');
+
+// Array of certificate objects
+export const certificates = [
+    { 
+        title: "Fundamentals", 
+        path: "/certificates/FUNDAMENTALS_PROGRAMMINGHUB_05-08-2024.jpg", 
+        tags: ["fundamentals", "programminghub"] 
+    },
+    { 
+        title: "C & PLT", 
+        path: "/certificates/C&PLT_SOFTECH_20-06-2024.jpg", 
+        tags: ["softech", "c", "plt"] 
+    },
+    { 
+        title: "NodeJS", 
+        path: "/certificates/NODEJS_PROGRAMMINGHUB_05-08-2024.jpg", 
+        tags: ["nodejs", "programminghub"] 
+    },
+    { 
+        title: "Python 3", 
+        path: "/certificates/PYTHON3_25-10-2024.jpg", 
+        tags: ["python3", "programminghub"] 
+    },
+    { 
+        title: "Docker", 
+        path: "/certificates/DOCKER_PROGRAMMINGHUB_25-10-2024.jpg", 
+        tags: ["docker", "programminghub", "ci/cd pipelines"] 
+    },    
+    { 
+        title: "IT Basics", 
+        path: "/certificates/ITBASICS_PROGRAMMINGHUB_27-10-2024.jpg", 
+        tags: ["it", "programminghub", "basics"] 
+    },
+];
