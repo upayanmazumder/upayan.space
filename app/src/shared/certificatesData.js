504bccd30eece4f2f82041bc346227ec1c291c96
upayanmazumder <upayanm3@gmail.com>
2024-11-23 11:53:10 +0530

    Use webp images for certs

--- app/src/shared/certificatesData.js
+++ app/src/shared/certificatesData.js
@@ -3,37 +3,37 @@ export const createSlug = (title) => title.toLowerCase().replace(/\s+/g, '-');
 export const certificates = [
     { 
         title: "Cookoff 9", 
-        path: "/certificates/COOKOFF9_CODECHEF_22-11-2024.png", 
+        path: "/certificates/COOKOFF9_CODECHEF_22-11-2024.webp", 
         tags: ["codechef", "cookoff", "competitive programming"] 
     },
     { 
         title: "Fundamentals", 
-        path: "/certificates/FUNDAMENTALS_PROGRAMMINGHUB_05-08-2024.jpg", 
+        path: "/certificates/FUNDAMENTALS_PROGRAMMINGHUB_05-08-2024.webp", 
         tags: ["fundamentals", "programminghub"] 
     },
     { 
         title: "C & PLT", 
-        path: "/certificates/C&PLT_SOFTECH_20-06-2024.jpg", 
+        path: "/certificates/C&PLT_SOFTECH_20-06-2024.webp", 
         tags: ["softech", "c", "plt"] 
     },
     { 
         title: "NodeJS", 
-        path: "/certificates/NODEJS_PROGRAMMINGHUB_05-08-2024.jpg", 
+        path: "/certificates/NODEJS_PROGRAMMINGHUB_05-08-2024.webp", 
         tags: ["nodejs", "programminghub"] 
     },
     { 
         title: "Python 3", 
-        path: "/certificates/PYTHON3_25-10-2024.jpg", 
+        path: "/certificates/PYTHON3_25-10-2024.webp", 
         tags: ["python3", "programminghub"] 
     },
     { 
         title: "Docker", 
-        path: "/certificates/DOCKER_PROGRAMMINGHUB_25-10-2024.jpg", 
+        path: "/certificates/DOCKER_PROGRAMMINGHUB_25-10-2024.webp", 
         tags: ["docker", "programminghub", "ci/cd pipelines"] 
     },    
     { 
         title: "IT Basics", 
-        path: "/certificates/ITBASICS_PROGRAMMINGHUB_27-10-2024.jpg", 
+        path: "/certificates/ITBASICS_PROGRAMMINGHUB_27-10-2024.webp", 
         tags: ["it", "programminghub", "basics"] 
     },
 ];
