e9786dea3aec93b9b030abb29860afe87c08763d
Upayan Mazumder <upayanm3@gmail.com>
2024-12-15 16:28:56 +0530

    Refactor layout structure and update theme color in manifest.json

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -1,49 +1,16 @@
-"use client"
+import RootLayoutClient from "./RootLayoutClient";
 
-import localFont from "next/font/local";
-import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import React from "react";
-import Header from "../components/header/header";
-import Navbar from "../components/navbar/navbar";
-import Footer from "../components/footer/footer";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export const metadata = {
+  title: "Upayan",
+  description: "Explore my page",
+};
 
 export default function RootLayout({ children }) {
-  React.useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((registration) => {
-          console.log("Service Worker registered with scope:", registration.scope);
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    }
-  }, []);
-
   return (
-    <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        <Navbar />
-        {children}
-        <Analytics />
-        <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <RootLayoutClient>{children}</RootLayoutClient>
       </body>
     </html>
   );
