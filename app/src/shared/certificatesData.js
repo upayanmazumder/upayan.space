ba68e81a0207134bc705b0ee5be73c6153462a1a
Upayan Mazumder <upayanm3@gmail.com>
2024-12-16 10:31:07 +0530

    updates

--- app/src/shared/certificatesData.js
+++ app/src/shared/certificatesData.js
@@ -1,39 +1,44 @@
 export const createSlug = (title) => title.toLowerCase().replace(/\s+/g, '-');
 
 export const certificates = [
-    { 
-        title: "Cookoff 9", 
-        path: "/certificates/COOKOFF9_CODECHEF_22-11-2024.webp", 
-        tags: ["codechef", "cookoff 9", "competitive programming", "coding contest", "gravitas","vit"] 
+    {
+        title: "Cookoff 9",
+        path: "/certificates/COOKOFF9_CODECHEF_22-11-2024.webp",
+        tags: ["codechef", "cookoff 9", "competitive programming", "coding contest", "gravitas", "vit"]
     },
-    { 
-        title: "Fundamentals", 
-        path: "/certificates/FUNDAMENTALS_PROGRAMMINGHUB_05-08-2024.webp", 
-        tags: ["fundamentals", "programminghub", "basics"] 
+    {
+        title: "Sensored 9",
+        path: "/certificates/SENSORED9_TEC_15-12-2024.webp",
+        tags: ["tec", "sensored 9", "iot", "electronics", "gravitas", "vit"]
     },
-    { 
-        title: "C and PLT", 
-        path: "/certificates/C&PROGRAMMINGINLOGICTECHNIC_SOFTECH_20-06-2024.webp", 
-        tags: ["softech", "c", "logic techniques", "programming basics"] 
+    {
+        title: "Fundamentals",
+        path: "/certificates/FUNDAMENTALS_PROGRAMMINGHUB_05-08-2024.webp",
+        tags: ["fundamentals", "programminghub", "basics"]
     },
-    { 
-        title: "NodeJS", 
-        path: "/certificates/NODEJS_PROGRAMMINGHUB_05-08-2024.webp", 
-        tags: ["node.js", "javascript backend", "programminghub", "server-side development"] 
+    {
+        title: "C and PLT",
+        path: "/certificates/C&PROGRAMMINGINLOGICTECHNIC_SOFTECH_20-06-2024.webp",
+        tags: ["softech", "c", "logic techniques", "programming basics"]
     },
-    { 
-        title: "Python 3", 
-        path: "/certificates/PYTHON3_25-10-2024.webp", 
-        tags: ["python 3", "programminghub", "python programming", "general-purpose language"] 
+    {
+        title: "NodeJS",
+        path: "/certificates/NODEJS_PROGRAMMINGHUB_05-08-2024.webp",
+        tags: ["node.js", "javascript backend", "programminghub", "server-side development"]
     },
-    { 
-        title: "Docker", 
-        path: "/certificates/DOCKER_PROGRAMMINGHUB_25-10-2024.webp", 
-        tags: ["docker", "containerization", "ci/cd pipelines", "devops tools", "programminghub"] 
-    },    
-    { 
-        title: "IT Basics", 
-        path: "/certificates/ITBASICS_PROGRAMMINGHUB_27-10-2024.webp", 
-        tags: ["it fundamentals", "information technology", "programminghub", "basics"] 
+    {
+        title: "Python 3",
+        path: "/certificates/PYTHON3_25-10-2024.webp",
+        tags: ["python 3", "programminghub", "python programming", "general-purpose language"]
+    },
+    {
+        title: "Docker",
+        path: "/certificates/DOCKER_PROGRAMMINGHUB_25-10-2024.webp",
+        tags: ["docker", "containerization", "ci/cd pipelines", "devops tools", "programminghub"]
+    },
+    {
+        title: "IT Basics",
+        path: "/certificates/ITBASICS_PROGRAMMINGHUB_27-10-2024.webp",
+        tags: ["it fundamentals", "information technology", "programminghub", "basics"]
     },
 ];
